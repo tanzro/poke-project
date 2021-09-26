630f140b640a461fbe1f8e46c0dae122455d1dce
tanzro <natan.vbrito@gmail.com>
2021-09-26 15:13:39 -0300

    added second screen on listing page and responsiveness

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './modules/home/home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomePageComponent },
-  { path: 'listing', loadChildren: () => import(`src/app/modules/listing/listing.module`).then(m => m.ListingModule) }
+  { path: '', component: HomePageComponent, pathMatch: 'full' },
+  { path: 'listing', loadChildren: () => import('../app/modules/listing/listing.module').then(m => m.ListingModule) }
 ];
 
 @NgModule({
